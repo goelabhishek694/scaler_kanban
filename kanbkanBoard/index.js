c5b7a9c224e97510981acadbbbdd3d775d33430a
Abhishek <abhishekgoel@gofynd.com>
2025-09-19 07:52:32 +0530

    delete ticket started

--- kanbkanBoard/index.js
+++ kanbkanBoard/index.js
@@ -1,88 +1,94 @@
 const addTaskBtn = document.querySelector(".add_btn");
 const modalCont = document.querySelector(".modal_cont");
 const modalTextArea = modalCont.getElementsByClassName("textarea_cont")[0];
-const modalPriorityColorCont = modalCont.getElementsByClassName("priority_color_cont")[0];
-const mainCont =  document.querySelector(".main_cont");
+const modalPriorityColorCont = modalCont.getElementsByClassName(
+  "priority_color_cont"
+)[0];
+const mainCont = document.querySelector(".main_cont");
 const priorityColors = ["pink", "blue", "purple", "green"];
 
 addTaskBtn.addEventListener("click", () => {
-    modalCont.style.display = "flex";
+  modalCont.style.display = "flex";
 });
 
 modalPriorityColorCont.addEventListener("click", (e) => {
-    if(e.target == e.currentTarget) return;
+  if (e.target == e.currentTarget) return;
 
-    let selectedPriority = e.target;
-    let allPriorityColors = modalPriorityColorCont.children;
-    
-    for(let i=0;i<allPriorityColors.length;i++){
-        allPriorityColors[i].classList.remove("active");
-    }
-    selectedPriority.classList.add("active");
-})
+  let selectedPriority = e.target;
+  let allPriorityColors = modalPriorityColorCont.children;
 
-modalCont.addEventListener("keypress", (e) =>{
-    if(e.key != "Enter") return;
-    //create a ticket
-    const content = modalTextArea.value;
-    const priorityColorEle = modalPriorityColorCont.getElementsByClassName("active")[0];
-    const priorityColor = priorityColorEle.classList[1];
-    const uid = new ShortUniqueId().rnd();
+  for (let i = 0; i < allPriorityColors.length; i++) {
+    allPriorityColors[i].classList.remove("active");
+  }
+  selectedPriority.classList.add("active");
+});
 
-    modalCont.style.display = "none";
-    createTicket(priorityColor, uid, content);
+modalCont.addEventListener("keypress", (e) => {
+  if (e.key != "Enter") return;
+  //create a ticket
+  const content = modalTextArea.value;
+  const priorityColorEle =
+    modalPriorityColorCont.getElementsByClassName("active")[0];
+  const priorityColor = priorityColorEle.classList[1];
+  const uid = new ShortUniqueId().rnd();
 
-    //reset modal
-    modalTextArea.value = "";
-    priorityColorEle.classList.remove("active");
-    modalPriorityColorCont.getElementsByClassName("blue")[0].classList.add("active");
+  modalCont.style.display = "none";
+  createTicket(priorityColor, uid, content);
 
-})
+  //reset modal
+  modalTextArea.value = "";
+  priorityColorEle.classList.remove("active");
+  modalPriorityColorCont
+    .getElementsByClassName("blue")[0]
+    .classList.add("active");
+});
 
-function createTicket(priorityColor, uid, content){
-    const ticketContainer = document.createElement("div");
-    ticketContainer.setAttribute("class", "ticket_cont");
-    ticketContainer.innerHTML = `<div class="ticket_color ${priorityColor}"></div>
+function createTicket(priorityColor, uid, content) {
+  const ticketContainer = document.createElement("div");
+  ticketContainer.setAttribute("class", "ticket_cont");
+  ticketContainer.innerHTML = `<div class="ticket_color ${priorityColor}"></div>
             <div class="ticket_id">#${uid}</div>
             <div class="ticket_area">${content}</div>
             <div class="lock_unlock">
                 <i class="fa-solid fa-lock"></i>
             </div>`;
-    const priorityColorEle = ticketContainer.querySelector(".ticket_color");
-    const lockUnlockBtn = ticketContainer.querySelector(".lock_unlock");
-    const ticketArea = ticketContainer.querySelector(".ticket_area");
-    addPriorityChangeListeners(priorityColorEle);
-    addLockUnlockListeners(lockUnlockBtn, ticketArea);
-    mainCont.appendChild(ticketContainer);
+  const priorityColorEle = ticketContainer.querySelector(".ticket_color");
+  const lockUnlockBtn = ticketContainer.querySelector(".lock_unlock");
+  const ticketArea = ticketContainer.querySelector(".ticket_area");
+  addPriorityChangeListeners(priorityColorEle);
+  addLockUnlockListeners(lockUnlockBtn, ticketArea);
+  deleteListeners();
+
+  mainCont.appendChild(ticketContainer);
 }
 
-function addPriorityChangeListeners(priorityColorEle){
-    console.log(priorityColorEle);
-    
-    priorityColorEle.addEventListener("click", function(){
-        let currColor = priorityColorEle.classList[1];
-        let currColorIdx = priorityColors.indexOf(currColor);
-        let nextColor = priorityColors[(currColorIdx+1)%priorityColors.length];
-        priorityColorEle.classList.remove(currColor);
-        priorityColorEle.classList.add(nextColor);
+function addPriorityChangeListeners(priorityColorEle) {
+  console.log(priorityColorEle);
 
-    });
-
+  priorityColorEle.addEventListener("click", function () {
+    let currColor = priorityColorEle.classList[1];
+    let currColorIdx = priorityColors.indexOf(currColor);
+    let nextColor = priorityColors[(currColorIdx + 1) % priorityColors.length];
+    priorityColorEle.classList.remove(currColor);
+    priorityColorEle.classList.add(nextColor);
+  });
 }
 
-function addLockUnlockListeners(lockUnlockBtn, ticketArea){
-    lockUnlockBtn.addEventListener("click", ()=>{
-        const button = lockUnlockBtn.children[0]?.classList[1];
-        //lock button -> unlock it and make ticketArea editable
-        if(button == "fa-lock"){
-            lockUnlockBtn.children[0].classList.remove("fa-lock");
-            lockUnlockBtn.children[0].classList.add("fa-unlock");
-            ticketArea.setAttribute("contenteditable", true);
-        }else{ //unlock button -> lock it and make ticketArea non-editable
-            lockUnlockBtn.children[0].classList.remove("fa-unlock");
-            lockUnlockBtn.children[0].classList.add("fa-lock");
-            ticketArea.setAttribute("contenteditable", false);
-        }
-    })
-
+function addLockUnlockListeners(lockUnlockBtn, ticketArea) {
+  lockUnlockBtn.addEventListener("click", () => {
+    const button = lockUnlockBtn.children[0]?.classList[1];
+    //lock button -> unlock it and make ticketArea editable
+    if (button == "fa-lock") {
+      lockUnlockBtn.children[0].classList.remove("fa-lock");
+      lockUnlockBtn.children[0].classList.add("fa-unlock");
+      ticketArea.setAttribute("contenteditable", true);
+    } else {
+      //unlock button -> lock it and make ticketArea non-editable
+      lockUnlockBtn.children[0].classList.remove("fa-unlock");
+      lockUnlockBtn.children[0].classList.add("fa-lock");
+      ticketArea.setAttribute("contenteditable", false);
+    }
+  });
 }
+
+function deleteListeners() {}
